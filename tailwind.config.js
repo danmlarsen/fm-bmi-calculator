611a9b8a3af61bbd0265af2fb75f4f92be1f5d0b
danmlarsen <larsendanmarius@gmail.com>
2024-09-18 15:32:26 +0200

    Structure and most styling completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif']
-      }
+    content: ['./index.html', './src/**/*.{js,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                blue: '#345FF6',
+                gunmetal: '#253347',
+                'dark-electric-blue': '#5E6E85',
+                skyblue: '#D6E6FE',
+                'blue-shadow': '#8FAECF',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
